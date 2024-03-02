1359e1b956db85b8f1b7d2b35dfd75e46f10a51b
Ashutosh0413 <patilashutosh67@gmail.com>
2024-03-02 17:34:05 +0530

    create, edit, delete functionalities implemented

--- script.js
+++ script.js
@@ -6,15 +6,16 @@ function onFormSubmit(e){
         insertNewRecord(formData);
     }
     else{
-
+        updateRecord(formData);
     }
+    resetForm();
     return false; 
 
 }
 
 //read form data
 
-function readFormData() {\
+function readFormData() {
     //store all the user submitted details as values in an object so it is easier to use them to populate the cells
     let formData = {};
     formData["buildingNumber"] = document.getElementById("buildingNumber").value;
@@ -44,5 +45,42 @@ function insertNewRecord(data) {
     cell6 = newRow.insertCell(5);
     cell6.innerHTML = fullAddress;
     cell7 = newRow.insertCell(6);
-    cell7.innerHTML = `<button>Edit</button> <button ">Delete</button>`; //Prompt the edit and delete buttons
+    cell7.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`; 
+    //Prompt the edit and delete buttons
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("buildingNumber").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("buildingName").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("city").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("landmark").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("pinCode").value = selectedRow.cells[4].innerHTML;
+    
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.buildingNumber;
+    selectedRow.cells[1].innerHTML = formData.buildingName;
+    selectedRow.cells[2].innerHTML = formData.city;
+    selectedRow.cells[3].innerHTML = formData.landmark;
+    selectedRow.cells[4].innerHTML = formData.pinCode;
+    let fullAddress = formData.buildingNumber + ' ' + formData.buildingName + ', ' + 'near ' + formData.landmark + formData.city  + '-' + formData.pinCode;
+    selectedRow.cells[5].innerHTML = fullAddress;
+}
+
+function onDelete(td) {
+    if (confirm('Do you want to delete this record?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById('storeList').deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+
+function resetForm() {
+    document.getElementById("buildingNumber").value = '';
+    document.getElementById("buildingName").value = '';
+    document.getElementById("city").value = '';
+    document.getElementById("landmark").value = '';
+    document.getElementById("pinCode").value = '';
+    selectedRow = null;
 }
