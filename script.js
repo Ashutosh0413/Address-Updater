1665862e299ee56f35d7f6a27109d3f3b1c0d2f2
Ashutosh0413 <patilashutosh67@gmail.com>
2024-03-02 16:35:13 +0530

    insertion function working and testing done, submission working checked

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
+let selectedRow = null;
+function onFormSubmit(e){
+    e.preventDefault;
+    let formData = readFormData();
+    if(selectedRow===null){
+        insertNewRecord(formData);
+    }
+    else{
 
-function onFormSubmit(){
+    }
+    return false; 
 
 }
 
 //read form data
 
-function readFormData() {
-    var formData = {};
+function readFormData() {\
+    //store all the user submitted details as values in an object so it is easier to use them to populate the cells
+    let formData = {};
     formData["buildingNumber"] = document.getElementById("buildingNumber").value;
     formData["buildingName"] = document.getElementById("buildingName").value;
     formData["city"] = document.getElementById("city").value;
     formData["landmark"] = document.getElementById("landmark").value;
     formData["pinCode"] = document.getElementById("pinCode").value;
     return formData;
+}
+
+//insert data into the table cells
+function insertNewRecord(data) {
+    let table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
+    let newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+		cell1.innerHTML = data.buildingNumber;
+    cell2 = newRow.insertCell(1);
+		cell2.innerHTML = data.buildingName;
+    cell3 = newRow.insertCell(2);
+		cell3.innerHTML = data.city;
+    cell4 = newRow.insertCell(3);
+		cell4.innerHTML = data.landmark;
+    cell5 = newRow.insertCell(4);
+	    cell5.innerHTML = data.pinCode;
+    //reading the full address of the user
+    let fullAddress = data.buildingNumber + ' ' + data.buildingName + ', ' + 'near ' + data.landmark + data.city  + '-' + data.pinCode;
+    cell6 = newRow.insertCell(5);
+    cell6.innerHTML = fullAddress;
+    cell7 = newRow.insertCell(6);
+    cell7.innerHTML = `<button>Edit</button> <button ">Delete</button>`; //Prompt the edit and delete buttons
 }
